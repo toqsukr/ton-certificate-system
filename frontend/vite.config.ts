d88f580b4f876ef67d526cfb9e62dfe183b107a5
Wastard <Ilya.pakylov@gmail.com>
2025-04-09 13:02:51 +0300

    init frontend & license

---
+++ frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import Unfonts from 'unplugin-fonts/vite'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      google: {
+        families: ['Open Sans', 'Roboto'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+})
