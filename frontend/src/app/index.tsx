c20b4bb09a0351086c4622687f883bc35688f67b
Wastard <Ilya.pakylov@gmail.com>
2025-04-25 13:58:22 +0300

    updated tonconnect manifest

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -5,9 +5,12 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { router } from './router.tsx'
 
+const testManifestURL =
+  'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl='http://localhost:5173/tonconnect-manifest.json'>
+    <TonConnectUIProvider manifestUrl={testManifestURL}>
       <RouterProvider router={router} />
     </TonConnectUIProvider>
   </StrictMode>
