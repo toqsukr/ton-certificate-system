78705603877a0417d4bb632ff7fd6045868bfc54
Wastard <Ilya.pakylov@gmail.com>
2025-04-30 02:51:46 +0300

    added auth localization

--- frontend/src/pages/auth/index.tsx
+++ frontend/src/pages/auth/index.tsx
@@ -2,22 +2,26 @@ import { Routes } from '@shared/model/routes'
 import Button from '@shared/uikit/button'
 import ContentField from '@shared/uikit/content-field'
 import { useTonConnectModal, useTonWallet } from '@tonconnect/ui-react'
+import { useTranslation } from 'react-i18next'
 import { Navigate } from 'react-router-dom'
 
 const AuthPage = () => {
   const wallet = useTonWallet()
   const { open } = useTonConnectModal()
+  const { t } = useTranslation()
 
   if (wallet) return <Navigate to={Routes.PROFILE} />
 
+  const authText = t('auth_unit_text').split(';')
+
   return (
-    <ContentField title='Authorization'>
+    <ContentField title={t('auth_unit_title')}>
       <p className='flex flex-col gap-2 mb-6'>
-        <span>Connect TON Wallet to access your TCS account.</span>
-        <span>No account yet? It will be created automatically after the Wallet is connected!</span>
-        <strong>Each TCS account may be connected with only single Wallet!</strong>
+        <span>{authText[0]}</span>
+        <span>{authText[1]}</span>
+        <strong>{authText[2]}</strong>
       </p>
-      <Button onClick={open}>Connect Wallet</Button>
+      <Button onClick={open}>{t('connect_wallet_button')}</Button>
     </ContentField>
   )
 }
