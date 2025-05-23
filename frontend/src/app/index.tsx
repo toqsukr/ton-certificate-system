0ac39de877921e5099db6877a4a132f47060e6be
Wastard <Ilya.pakylov@gmail.com>
2025-05-23 12:13:28 +0300

    commit cheating

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,6 +1,5 @@
 import { queryClient } from '@shared/api/query-client.ts'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { init } from '@telegram-apps/sdk-react'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -9,13 +8,6 @@ import './index.css'
 import './lib/i18next/i18next.ts'
 import { router } from './router.tsx'
 import './tonconnect.css'
-// import {} from '@telegram-apps/sdk-react'
-
-try {
-  init()
-} catch (e) {
-  console.log('outside telegram usage')
-}
 
 const testManifestURL =
   'https://raw.githubusercontent.com/toqsukr/ton-certificate-system/refs/heads/master/frontend/tonconnect-manifest.json'
